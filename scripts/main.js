7cb051cc0b7a0e30986f8f95707adf22db796b04
Elizabeth Dabbs <dabbs_elizabeth@bah.com>
2016-06-14 18:52:01 -0400

    design updates

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by elizabethdabbs
+ */
+jQuery(document).ready(function ($) {
+    var pathname = window.location.pathname;
+    console.log(pathname);
+    if (pathname == '/') {
+        $('.nav-home').addClass('active');
+    }
+    if (pathname == '/projects/') {
+        $('.nav-projects').addClass('active');
+    }
+    if (pathname == '/art/') {
+        $('.nav-artwork').addClass('active');
+    }
+    if (pathname == '/about/') {
+        $('.nav-about').addClass('active');
+    }
+    if (pathname == '/contact/') {
+        $('.nav-contact').addClass('active');
+    }
+});
+
