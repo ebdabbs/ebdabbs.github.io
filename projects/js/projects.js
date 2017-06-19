ec93cbe59ac266a6c700381a488dae52dfbcf46d
Elizabeth Salita <dabbs_elizabeth@bah.com>
2017-06-19 18:45:44 -0400

    code cleanup + new colors

---
+++ projects/js/projects.js
@@ -0,0 +1,11 @@
+/**
+ * Created by elizabethdabbs on 2/25/16.
+ */
+
+(function ($) {
+    $(document).ready(function(){
+        $("#dol").flip({
+            trigger: 'hover tap'
+        });
+    });
+}(jQuery));
